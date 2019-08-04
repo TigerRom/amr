f6819038e0322c37cd694155fbca9aa89d58d18f
mldn <tjh1289024322@163.com>
2019-08-04 13:11:19 +0800

    Change Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,5 @@
 public class Hello{
 	public static void main(String[] args) {
-		System.out.println("Hello Git") ;
-		System.out.println("hello ,what id your name") ;
-		System.out.println("i am fine ,thank you") ;
-		System.out.println("nihao") ;
+		System.out.println("Hello World") ;
 	} 
 }
