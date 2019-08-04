4ee56677a6d565e23f1da48658f8cf436065b98d
mldn <tjh1289024322@163.com>
2019-08-04 12:32:23 +0800

    Change Hello.java Add Two Lines

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
 	public static void main(String[] args) {
 		System.out.println("Hello Git") ;
+		System.out.println("hello ,what id your name") ;
+		System.out.println("i am fine ,thank you") ;
 	} 
 }
