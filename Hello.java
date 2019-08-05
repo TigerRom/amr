e8ff0bff7f86eeb53dfdf1f84310213ef73e8ce0
mldn <tjh1289024322@163.com>
2019-08-05 17:01:53 +0800

    Change Hello.java

--- Hello.java
+++ Hello.java
@@ -2,10 +2,5 @@ public class Hello{
 	public static void main(String[] args) {
 		Runtime run = Runtime.getRuntime() ;
 		run.gc() ;
-		System.out.println("Hello World") ;
-		System.out.println("www.jixianit.com") ;
-		System.out.println("www.jixianit.com") ;
-		System.out.println(") ;
-		System.out.println("www.jixianit.com") ;
 	} 
 }
