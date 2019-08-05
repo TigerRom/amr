09f871e7430251da6b4e7fbca2f699363e039ccf
mldn <tjh1289024322@163.com>
2019-08-05 14:45:24 +0800

    Stash file

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello{
 		System.out.println("Hello World") ;
 		System.out.println("www.jixianit.com") ;
 		System.out.println("www.jixianit.com") ;
-		System.out.println("www.jixianit.com") ;
+		System.out.println(") ;
 		System.out.println("www.jixianit.com") ;
 	} 
 }
