cecbaa61f807b1ad891809bdb41cf060db140a76
mldn <tjh1289024322@163.com>
2019-08-05 12:06:30 +0800

    Change One File Hello.java,Add One File Admin.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,7 @@
 public class Hello{
 	public static void main(String[] args) {
 		System.out.println("Hello World") ;
-		System.out.println("Hello World") ;
-		System.out.println("Hello World") ;
-		System.out.println("Hello World") ;
-		System.out.println("Hello World") ;
+		System.out.println("www.jixianit.com") ;
+		System.out.println("世界和平，在舞着那种") ;
 	} 
 }
