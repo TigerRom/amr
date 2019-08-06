a657bdb1d282f06c4cca3664838dae74d13bc189
mldn <tjh1289024322@163.com>
2019-08-06 09:42:24 +0800

    Modify Change Admin.java

--- Admin.java
+++ Admin.java
@@ -4,9 +4,6 @@ public class Admin implements Serializable
 {
 	private Stringf aid ;
 	private String passwrid ;
-<<<<<<< HEAD
 	private Groups groups ;
-=======
 	private List<Role> roles ;
->>>>>>> 1419039c213727b16fdb1784be39ae68e491aaed
 }
